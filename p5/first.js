43b0bad80686ab503d26fc447a53ff3b587c1cdd
BaeMax <advayagoel17@hotmail.com>
2017-04-02 09:16:07 +0530

    Update first.js

--- p5/first.js
+++ p5/first.js
@@ -1,9 +1,58 @@
 function setup() { 
-  createCanvas(400, 400);
-}
-
+  createCanvas(500, 400);
+} 
+var y=0
+var x=100
 function draw() { 
-   background(220);
-   ellipse(10, 10, 10);
+  background(0,0,0);
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>20) {}
+else {ellipse(120,150,15,15)
+ }
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>40) {}
+else {ellipse(140,150,15,15)
+ 
+ }
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>60) {}
+else {ellipse(160,150,15,15)
 }
-
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>80) {}
+else {ellipse(180,150,15,15)
+}
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>100) {}
+else {ellipse(200,150,15,15)
+}
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>120) {}
+else {ellipse(220,150,15,15)
+}
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>140) {}
+else {ellipse(240,150,15,15)
+}
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>160) {}
+else {ellipse(260,150,15,15)
+}
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>180) {}
+else {ellipse(280,150,15,15)
+}
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>200) {}
+else {ellipse(300,150,15,15)
+}
+arc(x,150,50,50,PI/4,-PI/4,PIE)
+if (y>220) {}
+else {ellipse(320,150,15,15)
+ 
+ }
+arc(x,150,100,100,PI/4,-PI/4,PIE)
+y=y+0.5
+x=x+0.5
+fill('yellow')
+}
